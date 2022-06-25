cfe16b58675af9400d9b2907e15f3e034bb0c605
bufferflies <1045931706@qq.com>
2022-06-25 14:41:33 +0800

    mod
    Signed-off-by: bufferflies <1045931706@qq.com>

--- example/components/codec/src/json/mod.rs
+++ example/components/codec/src/json/mod.rs
@@ -3,8 +3,16 @@ pub use json::print_codec;
 
 #[cfg(test)]
 mod tests{
+    use itertools::Itertools;
     #[test]
-    fn test_print_codec(){
-        super::print_codec();
+    fn test_print_codec(){ 
+        let data = vec![1.0];
+        let d=data.into_iter().coalesce(|pre, cur|
+            if pre<cur {
+                Err((pre , cur))
+           } else {
+               Ok(pre)
+            });
+        itertools::assert_equal(d,vec![1.0]);
     }
 }
